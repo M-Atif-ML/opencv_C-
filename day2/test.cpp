2683a9a72ded4628bd17fbf00b2672f4a1ad605b
Atif Khan <wahabshah10200@gmail.com>
2025-07-28 23:27:24 +0500

    worked on some more basic functions

--- day2/test.cpp
+++ day2/test.cpp
@@ -4,17 +4,27 @@
 
 using namespace std;
 using namespace cv;
+// basic functions 
 int main() {
     string path = "download.jpeg";
     Mat img = imread(path);
-    Mat imgGray,blur,canny;
+    Mat imgGray,blur,canny,imgDil,erodImg;
     
     cvtColor(img,imgGray,COLOR_BGR2GRAY);
     GaussianBlur(img,blur,Size(7,7),5,0);
-    Canny(img,canny,50,150);
+    Canny(blur,canny,50,150);
+    
+    Mat kernel = getStructuringElement(MORPH_RECT,Size(5,5));
+    dilate(canny,imgDil,kernel);
+    
+    erode(imgDil,erodImg,kernel);
+    
+    
     imshow("Image Gray",imgGray);
     imshow("Blur: ",blur);
     imshow("Canny: ",canny);
+    imshow("Dilated image: ",imgDil);
+    imshow("Eroded image:",erodImg);
     
     waitKey(0);
 }
